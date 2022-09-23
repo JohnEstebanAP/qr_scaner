e4203005c085d0a84fd15332f34da1825dd2fd39
John Esteban Alvarez Piedrahita <john.alvarez@sofka.com.co>
2022-09-23 16:37:30 -0500

    add mapa

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -1,21 +1,69 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../model/scan_model.dart';
 
-class MapaPage extends StatelessWidget {
+class MapaPage extends StatefulWidget {
   const MapaPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<MapaPage> createState() => _MapaPageState();
+}
 
-    final scan = ModalRoute.of(context)?.settings.arguments;
+class _MapaPageState extends State<MapaPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    ScanModel? scan = ModalRoute.of(context)?.settings.arguments as ScanModel?;
+
+    final CameraPosition puntoInecial = CameraPosition(
+      target: scan!.getLatLng(),
+      zoom: 17,
+      tilt: 50,
+    );
+
+    //Marcadores
+    Set<Marker> markers = new Set<Marker>();
+    markers.add(new Marker(
+      markerId: MarkerId('geo-location'),
+      position: scan.getLatLng(),
+    ));
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Mapa'),
+        title: const Text('Mapa'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.location_on_outlined),
+            onPressed: () async {
+              final GoogleMapController controller = await _controller.future;
+              controller
+                  .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                target: scan.getLatLng(),
+                zoom: 17,
+                tilt: 50,
+              )));
+            },
+          ),
+        ],
       ),
-      body: Center(),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        markers: markers,
+        initialCameraPosition: puntoInecial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.layers),
+        onPressed: (){
 
+        },
+      ),
     );
   }
 }

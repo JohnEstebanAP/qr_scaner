14d237a584ed784510c4762fed6af247423ca010
John Esteban Alvarez Piedrahita <john.alvarez@sofka.com.co>
2022-09-23 11:44:09 -0500

    Scaner part 1

--- lib/src/widgets/scan_buttom.dart
+++ lib/src/widgets/scan_buttom.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButtom extends StatelessWidget {
   const ScanButtom({Key? key}) : super(key: key);
@@ -10,8 +10,8 @@ class ScanButtom extends StatelessWidget {
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
       onPressed: () async{
-       // String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Canselar', false, ScanMode.QR);
-       // print(barcodeScanRes);
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Canselar', false, ScanMode.QR);
+        print(barcodeScanRes);
       },
     );
   }

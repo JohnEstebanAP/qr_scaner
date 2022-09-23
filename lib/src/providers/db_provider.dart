abeab324f3216d10ee7789be607589e3ba8e2266
John Esteban Alvarez Piedrahita <john.alvarez@sofka.com.co>
2022-09-23 12:10:51 -0500

    Qr-scaner parte 2 and path_provider bug

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,11 +1,11 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
-//import 'package:path_provider/path_provider.dart';
-//import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DBProvider{
-/*
+
   static Database? _database;
   static final DBProvider  db = DBProvider._();
 
@@ -38,5 +38,5 @@ class DBProvider{
          ''');
        }
      );
-  }*/
+  }
 }
